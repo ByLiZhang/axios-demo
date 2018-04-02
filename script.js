28e0cfeef4fdff78ebd5ddd2e67ce7e458327be8
Li Zhang <bylizhang@gmail.com>
2018-04-02 11:54:11 -0700

    Demo of using axios

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const BASE_URL = 'http://api.reactprototypes.com';
 const API_KEY = '?key=testuser1234';
 
 axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
-    const { todos } = resp.data;
-    const table = $('table tbody');
-
-    addToDom(todos, table);
+	console.log('Server Response:', resp);
+}).catch(err => {
+	console.log('There was an error:', err.message);
 });
 
-const addToDom = (list, container) => {
-    const tableRows = list.map((item, index) => {
-
-        const tableData = [
-            $(`<td>${index + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
-
-        return $('<tr>').append(tableData);
-    });
-
-    container.append(tableRows);
+const newItem = {
+	title: 'Li\'s new item',
+	details: 'practising Axios'
 };
+
+// axios.post(`${BASE_URL}/todos${API_KEY}`, newItem).then(resp => {
+// 	console.log('Add Response:', resp);
+// });
+// 
+// _id: '5ac278f6329150131fbbff6c'
+
+const itemId = '5ac278f6329150131fbbff6c';
+
+axios.get(`http://api.reactprototypes.com/todos/${itemId}${API_KEY}`).then(resp => {
+	console.log('Server Response using item ID:', resp);
+})
